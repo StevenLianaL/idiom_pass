c08335d3a4454a14255a6c825b559214e36678e1
wz <wangzhou8284@outlook.com>
2020-01-07 09:30:30 +0800

    handle repeat

--- zero.py
+++ zero.py
@@ -19,34 +19,38 @@ def get_match_idioms(word: str):
         return res
 
 
+def get_word(res: OrderedDict):
+    tail_words = res[list(res.keys())[-1]]
+    while True:
+        if not tail_words:
+            res.popitem()
+            tail_words = res[list(res.keys())[-1]]
+        else:
+            break
+    return tail_words.pop()
+
+
 # 1. 运行 ok
 # 2. 错误检查
 # 3. 无法查询后退 ok
-# 4. 重复词处理
+# 4. 重复词处理 ok
 # 5. 最长备份
-def auto_idiom(w: str):
-    results, backup = OrderedDict(), None
-    while True:
-        try:
-            words = get_match_idioms(w)
-            if words:
-                results[w] = words[:-1]  # in
-            else:
-                if tail_words := results[list(results.keys())[-1]]:  # 处理无法接龙，新词重来
-                    w = tail_words.pop()
-                    continue
-                else:
-                    results.popitem()
-                    w = results[list(results.keys())[-1]].pop()
-                    continue
-            print(f"{w=}")
-            print(f"{results.keys()=}")
-            w = words[-1]  # new word
-        except Exception as e:
-            print(f"{type(e)=},{e=}")
-            print(list(results.items())[-2:])
-            raise ValueError(f"new word {w=}")
+def auto_idiom(w: str = '啊一', limit: int = 1000):
+    results = OrderedDict()
+    for i in range(limit):
+        if w in results.keys():
+            w = get_word(results)
+            continue
+        words = get_match_idioms(w)
+        if words:
+            results[w] = words[:-1]  # in
+        else:
+            w = get_word(results)
+            continue
+        w = words[-1]  # new word
+    return results.keys()
 
 
 if __name__ == '__main__':
-    auto_idiom('啊鬼')
+    for i in auto_idiom():
+        print(i)
